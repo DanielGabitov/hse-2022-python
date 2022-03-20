1cef96d3f32307e9efd30144ba152e958820bcbd
Daniel Gabitov <work.gabitov@gmail.com>
2022-03-20 23:32:27 +0300

    fix remarks

--- hw_05/easy.py
+++ hw_05/easy.py
@@ -1,22 +1,28 @@
 import os
 import sys
-import time
-import random
 import aiohttp
 import asyncio
 import aiofiles
 
+
 URL = 'https://picsum.photos/200'
 
 
-async def routine(*, n: int, path: str):
+async def routine(*, session: aiohttp.ClientSession, path: str, img_name: str):
+    async with session.get(URL) as response:
+        content = await response.read()
+        img_path = os.path.join(path, img_name)
+        async with aiofiles.open(img_path, mode='bw') as file:
+            await file.write(content)
+            await file.close()
+
+
+async def main_routine(*, n: int, path: str):
     async with aiohttp.ClientSession() as session:
-        for i in range(n):
-            async with session.get(URL) as response:
-                img_path = os.path.join(path, f'img_{i}.jpg')
-                async with aiofiles.open(f'{img_path}', 'bw') as file:
-                    await file.write((await response.read()))
-                time.sleep(random.random())
+        await asyncio.gather(
+            *((routine(session=session, path=path, img_name=f"img_{i}.jpg"))
+              for i in range(n))
+        )
 
 
 if __name__ == "__main__":
@@ -24,4 +30,4 @@ if __name__ == "__main__":
     folder_path: str = sys.argv[2]
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    asyncio.run(routine(n=img_number, path=folder_path))
+    asyncio.run(main_routine(n=img_number, path=folder_path))
